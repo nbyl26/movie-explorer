88fc752acd3e842e09f44862ff74820c8382d26c
Nabil <nabilpasha230606@gmail.com>
2025-07-04 20:55:39 +0700

    chore: initialize project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-3xl font-bold">
+        Hello Movie Explorer!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
